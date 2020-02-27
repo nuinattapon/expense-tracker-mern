c3224420dad658d41a43040c5b9cec439f84b9e1
Nattapon Sub-Anake <nattapon.subanake@gmail.com>
2020-02-27 08:06:25 +0700

    First commit

--- server.js
+++ server.js
@@ -1,33 +1,44 @@
-const path = require('path');
-const express = require('express');
-const dotenv = require('dotenv');
-const colors = require('colors');
-const morgan = require('morgan');
-const connectDB = require('./config/db');
+const path = require('path')
+const express = require('express')
+const dotenv = require('dotenv')
+const colors = require('colors')
+const morgan = require('morgan')
+const connectDB = require('./config/db')
 
-dotenv.config({ path: './config/config.env' });
+dotenv.config({ path: './config/config.env' })
+// process.env.NODE_ENV = 'production'
+// process.env.PORT = 5000
+// process.env.MONGO_URI =
+//   'mongodb+srv://nui:Otpqgjw0JfpWdgFV@nattapon-cebnd.mongodb.net/test?retryWrites=true&w=majority'
 
-connectDB();
+// console.log(process.env.MONGO_URI)
+connectDB()
 
-const transactions = require('./routes/transactions');
+const transactions = require('./routes/transactions')
 
-const app = express();
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
 
-if(process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
 }
 
-app.use('/api/v1/transactions', transactions);
+app.use('/api/v1/transactions', transactions)
 
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
 
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  )
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
-
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+)
